678d4cf49c253b39c11863303b322e8e11c96a92
widanta <widanta66@gmail.com>
2021-11-09 22:12:03 +0800

    menambahkan warna

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 $(document).ready(function () {
-  // seleksi dulu window
-  $(window).scroll(() => {
-    // selksi scroll ke atas
-    const scroll = $(window).scrollTop();
+  // // // seleksi dulu window
+  // var source = "/lagu.mp3";
+  // var audio = document.createElement("audio");
+  // audio.load();
+  // audio.addEventListener(
+  //   "load",
+  //   function () {
+  //     audio.play();
+  //   },
+  //   true
+  // );
+  // audio.src = source;
+  // $(window).scroll(() => {
+  //   // selksi scroll ke atas
+  //   const scroll = $(window).scrollTop();
+  //   // jika tinggi scroll lewat dari 300 tampilkan bg
+  //   if (scroll >= 500) {
+  //     audio.play();
+  //   } else {
+  //     audio.pause();
+  //     audio.currentTime = 0;
+  //   }
+  // });
+  // $("#playBtn").click(function () {
+  //   audio.play();
+  // });
+  // $("#pauseBtn").click(function () {
+  //   audio.pause();
+  // });
+  // $("#stopBtn").click(function () {
+  //   audio.pause();
+  //   audio.currentTime = 0;
+  // });
+  // baru
 
-    // jika tinggi scroll lewat dari 300 tampilkan bg
-    if (scroll >= 500) {
-      $("#lagu").show();
-    } else {
-      $("#lagu").hide();
+  var playing = false;
+  // var audioElm = $("#lagu").get(0);
+  var audioElm = "/lagu.mp3";
+
+  $(window).scroll(function () {
+    var pageScroll = $(window).scrollTop();
+    if (!playing && pageScroll > 500 && pageScroll < 3000) {
+      audioElm.play();
+      playing = true;
+    } else if (pageScroll > 3000 || pageScroll < 500) {
+      audioElm.pause();
+      playing = false;
     }
   });
 });
